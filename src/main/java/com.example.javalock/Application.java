f9cfdd07c137b833093773132e4f78a62591aa18
leejinseok <sonaky47@gmail.com>
2023-11-23 10:15:33 +0900

    init

---
+++ src/main/java/com.example.javalock/Application.java
@@ -0,0 +1,7 @@
+package com.example.javalock;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/example/javalock/ApplicationTest.java
@@ -0,0 +1,6 @@
+package com.example.javalock;
+
+
+class ApplicationTest {
+
+}
